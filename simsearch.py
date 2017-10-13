ea923faeb30ded776fcfc7d943afded58d5ac21f
jianyu-m <jianyu@hku.hk>
2017-10-13 13:51:36 +0800

    init

--- simsearch.py
+++ simsearch.py
@@ -9,7 +9,6 @@ def process_input():
     return mat.reshape((l, 33))[:100, 1:]
 
 if __name__ == "__main__":
-
     k = 10
     mat = process_input()
     avg = numpy.mean(mat, axis=0)
@@ -22,8 +21,8 @@ if __name__ == "__main__":
     p.dimension = k
     idxkd = index.Index('3d_index', properties=p)
     for idx, r in enumerate(B):
-        idxkd.insert(idx + 2, numpy.concatenate([r, r]), r)
-    for i in idxkd.nearest(numpy.concatenate([B[0], B[0]])):
-        print(i)
-    print(B[0])
+        idxkd.insert(idx + 1, numpy.concatenate([r, r]), r)
 
+    br = numpy.dot(mat[1], numpy.transpose(v))[:k]
+    for i in idxkd.nearest(numpy.concatenate([br, br])):
+        print(i)
